3134c49776a27f2af2109b853d347a1a77f9ea2b
Sadaf Halim <acesadaf@gmail.com>
2020-08-08 19:34:04 -0500

    added event listener, panning to pin

--- src/Location.js
+++ src/Location.js
@@ -57,6 +57,7 @@ export class CurrentLocation extends React.Component {
       const node = ReactDOM.findDOMNode(mapRef);
 
       let { zoom } = this.props;
+      let { minZoom, maxZoom } = this.props;
       const { lat, lng } = this.state.currentLocation;
       const center = new maps.LatLng(lat, lng);
       const mapConfig = Object.assign(
@@ -64,11 +65,111 @@ export class CurrentLocation extends React.Component {
         {
           center: center,
           zoom: zoom,
+          minZoom: minZoom,
+          maxZoom: maxZoom,
+          styles: [
+            { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+            {
+              elementType: "labels.text.stroke",
+              stylers: [{ color: "#242f3e" }],
+            },
+            {
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#746855" }],
+            },
+            {
+              featureType: "administrative.locality",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#d59563" }],
+            },
+            {
+              featureType: "poi",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#d59563" }],
+            },
+            {
+              featureType: "poi.park",
+              elementType: "geometry",
+              stylers: [{ color: "#263c3f" }],
+            },
+            {
+              featureType: "poi.park",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#6b9a76" }],
+            },
+            {
+              featureType: "road",
+              elementType: "geometry",
+              stylers: [{ color: "#38414e" }],
+            },
+            {
+              featureType: "road",
+              elementType: "geometry.stroke",
+              stylers: [{ color: "#212a37" }],
+            },
+            {
+              featureType: "road",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#9ca5b3" }],
+            },
+            {
+              featureType: "road.highway",
+              elementType: "geometry",
+              stylers: [{ color: "#746855" }],
+            },
+            {
+              featureType: "road.highway",
+              elementType: "geometry.stroke",
+              stylers: [{ color: "#1f2835" }],
+            },
+            {
+              featureType: "road.highway",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#f3d19c" }],
+            },
+            {
+              featureType: "transit",
+              elementType: "geometry",
+              stylers: [{ color: "#2f3948" }],
+            },
+            {
+              featureType: "transit.station",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#d59563" }],
+            },
+            {
+              featureType: "water",
+              elementType: "geometry",
+              stylers: [{ color: "#17263c" }],
+            },
+            {
+              featureType: "water",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#515c6d" }],
+            },
+            {
+              featureType: "water",
+              elementType: "labels.text.stroke",
+              stylers: [{ color: "#17263c" }],
+            },
+          ],
         }
       );
 
       this.map = new maps.Map(node, mapConfig);
+      this.map.addListener("click", (event) => {
+        alert("clicked" + event.latLng);
+        this.placeMarkerAndPanTo(event.latLng, this.map, maps);
+      });
     }
+  }
+
+  placeMarkerAndPanTo(latLng, map, maps) {
+    var marker = new maps.Marker({
+      position: latLng,
+      map: map,
+    });
+    map.panTo(latLng);
   }
 
   renderChildren() {
@@ -114,13 +215,13 @@ export class CurrentLocation extends React.Component {
 export default CurrentLocation;
 
 CurrentLocation.defaultProps = {
-  zoom: 14,
+  zoom: 7,
   initCenter: {
     lat: 32.78,
     lng: -96.8,
   },
   minZoom: 5,
-  maxZoom: 10,
+  maxZoom: 8,
   centerAroundCurrentLocation: false,
   visible: true,
 };
