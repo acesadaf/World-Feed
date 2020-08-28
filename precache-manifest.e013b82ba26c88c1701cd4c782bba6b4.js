40d154a68cf71dae3e4617ffd99a6a6d99ce85e8
Sadaf Halim <acesadaf@gmail.com>
2020-08-27 22:37:13 -0500

    Updates

---
+++ precache-manifest.e013b82ba26c88c1701cd4c782bba6b4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b86dd657992a5b542beffe52ca69e0c6",
+    "url": "/World-Feed/index.html"
+  },
+  {
+    "revision": "bed8c651a016c86e1ce5",
+    "url": "/World-Feed/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "7f429a7236ba8a8e6ec4",
+    "url": "/World-Feed/static/css/main.932f5ad3.chunk.css"
+  },
+  {
+    "revision": "bed8c651a016c86e1ce5",
+    "url": "/World-Feed/static/js/2.9f789487.chunk.js"
+  },
+  {
+    "revision": "d3eab2c800484648f446253871549a7a",
+    "url": "/World-Feed/static/js/2.9f789487.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f429a7236ba8a8e6ec4",
+    "url": "/World-Feed/static/js/main.2a987a43.chunk.js"
+  },
+  {
+    "revision": "637b144c05bba6620c43",
+    "url": "/World-Feed/static/js/runtime-main.6deb0bc8.js"
+  },
+  {
+    "revision": "e4b1dac8913cc81ac8deb55ac6e0d451",
+    "url": "/World-Feed/static/media/cartoon-globe.e4b1dac8.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/World-Feed/static/media/logo.5d5d9eef.svg"
+  }
+]);
